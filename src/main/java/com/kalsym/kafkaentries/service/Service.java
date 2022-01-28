b3bd62aff1fa245ba03830f812c06014204500b2
HasanSajjadSwati <swati.hasansajjad@gmail.com>
2022-01-28 11:17:48 +0500

    Fixed some bugs

--- src/main/java/com/kalsym/kafkaentries/service/Service.java
+++ src/main/java/com/kalsym/kafkaentries/service/Service.java
@@ -19,23 +19,26 @@ import org.apache.kafka.common.serialization.StringSerializer;
 @org.springframework.stereotype.Service
 public class Service {
 
-    private static final String TOPIC = "abc";
+    private static final String TOPIC = "topic";
     private static final String GROUP = "demoGroup";
     private static final int RECORDS = 50000000;
     private final Properties producerProps = new Properties();
     private final Properties consumerProps = new Properties();
-
+    private KafkaProducer<String, byte[]> producer;
+    
     public String insertValues() throws InterruptedException {
+        int i = 0;
         producerProps.setProperty("bootstrap.servers", "localhost:9092");
         producerProps.setProperty("kafka.topic.name", TOPIC);
-        KafkaProducer<String, byte[]> producer = new KafkaProducer<>(this.producerProps, new StringSerializer(), new ByteArraySerializer());
-        for (int i = 0; i < RECORDS; i++) {
+        producer = new KafkaProducer<>(this.producerProps, new StringSerializer(), new ByteArraySerializer());
+        for (; i < RECORDS; i++) {
             byte[] payload = ("Value# " + i).getBytes();
             ProducerRecord<String, byte[]> record = new ProducerRecord<>(producerProps.getProperty("kafka.topic.name"), payload);
             producer.send(record);
+            
         }
         producer.close();
-        return "Finished Insertion Into Topic: "+ TOPIC;
+        return i + " values inserted";
     }
 
     public List<String> fetchValues() throws InterruptedException {
@@ -43,24 +46,22 @@ public class Service {
 
         consumerProps.put("bootstrap.servers", "localhost:9092");
         consumerProps.put("group.id", GROUP);
-//        consumerProps.put("auto.commit.interval.ms", "1000");
+        consumerProps.put("auto.commit.interval.ms", "1000");
         consumerProps.put("max.poll.records", "50000000");
         consumerProps.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         consumerProps.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-
         try ( KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProps);) {
             consumer.subscribe(Arrays.asList(TOPIC));
 
-            for (int i = 0; i < RECORDS; i++) {
-                ConsumerRecords<String, String> records = consumer.poll(1);
+            for (int i = 0; i < 5000000; i++) {
+                ConsumerRecords<String, String> records = consumer.poll(0);
                 for (ConsumerRecord<String, String> record : records) {
                     allRecords.add(record.value());
-                    //System.out.println("Received Message: " + record.value());
+//                    System.out.println("Fetching Record At Index = "+ i +" Value = " + record.value());
                 }
 
             }
             System.out.println("Finshed Fetching!");
-
             consumer.close();
         }
         return allRecords;
